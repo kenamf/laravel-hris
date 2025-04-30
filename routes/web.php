0dcf472044fe35f3c8acb8f4462500789de60e4d
Fatur Kenam <faturgoo@gmail.com>
2025-04-30 20:21:27 +0700

    Setup Laravel project for GitHub

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\DashboardController;
+use App\http\Controllers\TaskController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('/tasks', TaskController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
